865327cee1c138b5a16223074b2f5054755019c5
Neil Empey <argus.two.2@gmail.com>
2020-04-14 19:35:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+	arr = []
+	array.each do |ele|
+		arr << badge_maker(ele)
+	end
+	return arr
+end
+
+def assign_rooms(array)
+	arr = []
+	array.each_with_index do |ele, i|
+		arr << "Hello, #{ele}! You'll be assigned to room #{i + 1}!"
+	end
+	arr
+end
+
+def printer(array)
+	x = 0
+	b = batch_badge_creator(array)
+	while x <= batch_badge_creator(array).size()
+ 	puts b[x]
+ x += 1
+end
+y = 0
+a = assign_rooms(array)
+while y <= assign_rooms(array).size()
+	puts a[y]
+	y += 1
+end
+end
