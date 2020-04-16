2f451a04eb73e8ef45eb073258d7fd44f7a1eccd
Jaeson Watts <illender.the.great@gmail.com>
2020-04-15 21:20:30 -0600

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,29 @@
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-  
+def batch_badge_creator(attendees)
+  counter = 0
+  attendees.each do |person|
+    attendees[counter] = badge_maker(person)
+    counter += 1
+  end
 end
 
-def assign_rooms
-  
+def assign_rooms(attendees)
+  counter = 0
+  attendees.each do |person|
+    attendees[counter] = "Hello, #{person}! You'll be assigned to room #{counter+1}!"
+    counter += 1
+  end
 end
 
-def printer
-  
+def printer(attendees)
+  counter = 0
+  attendees.each do |person|
+    puts badges[counter]
+    puts room_assignments[counter]
+    counter += 1
+  end
 end
