470cbf93f9c02de0ed417cb71238643c4d39c276
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-21 21:28:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  attendees = []
+  name.each do |badges|
+  attendees << "Hello, my name is #{badges}."
+  end
+  attendees
+end
+
+def assign_rooms(name)
+  name.each_with_index.collect do |presenter, classify|
+  "Hello, #{presenter}! You'll be assigned to room #{classify+1}!"
+  end
+end
+
+def printer(name)
+  batch_badge_creator(name).each do |badges|
+    puts badges
+  end
+
+  assign_rooms(name).each do |placement|
+    puts placement
+  end
+end
